20652ac69ca74505f12b57f502a4a869561dd7ac
artusvranken <artusvranken@gmail.com>
2019-10-31 10:40:18 +0100

    Initial commit

---
+++ src/main/java/com/github/artus/cicdtutorial/CicdTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.github.artus.cicdtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/artus/cicdtutorial/CicdTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.artus.cicdtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
